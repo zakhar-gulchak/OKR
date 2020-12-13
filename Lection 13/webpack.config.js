42126bc0b2961e3e5944862a2e8f70a00358bb26
Zahar Gulchak <gulchak.zahar@gmail.com>
2020-12-13 23:38:10 +0200

    Initial commit

---
+++ Lection 13/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './scripts/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        liveReload: true,
+        hot: true,
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "img", to: "img" },
+            ],
+        }),
+    ]
+};
